f0e062a34bdebbd27f782d8f6a2c7ff6130b23fe
YeranosyanNarek <narekyeranosyan1111@gmail.com>
2018-10-02 14:26:21 +0400

    Homework2_27-09-2018_1-2

---
+++ Homework2_27-09-2018_Shahane_1.py
@@ -0,0 +1,15 @@
+
+
+list1 = [3, 6, True, True, -1, 'abc', (1, 2), [2, 3], 6]
+ 
+for i in range(len(list1)):
+    if type(list1[i]) == tuple:
+        print(i)
+        break      
+
+
+
+
+
+
+
---
+++ Homework2_27-09-2018_Shahane_2.py
@@ -0,0 +1,27 @@
+     
+print('selekt username')
+username = input()
+while username != "Batman":
+    
+    print('selekt username')
+    username = input()
+    
+    if username == 'Batman':
+        break
+        
+print('selekt password')
+password = input()
+while username != "Batman":
+    
+    print('selekt username')
+    username = input()
+    
+    if username == 'I am Batman':
+        break
+print('welcome')
+
+
+
+
+
+
