0045c3435dbb88173df8fb0d29ea2c6ed5f7bf3a
YeranosyanNarek <narekyeranosyan1111@gmail.com>
2018-10-14 22:55:50 +0400

    Homework_classes_RevA

--- Homework_classes.py
+++ Homework_classes.py
@@ -1,29 +1,19 @@
-
 class Person:
-
     def __init__(self, name, last_name, age, gender, student):
         self.name = name
         self.last_name = last_name
         self.age = age
         self.gender = gender
         self.student = student
-
-    def Greeting(self):
-        return ("Welcome dear ", self.name)
-
+    def Greeting(self, second_person):
+        return ('Welcome dear', self.name)
     def Goodbye(self):
         return ('Bye everyone!')
-
-    def Favourite_num(self):
+    def Favourite_num(self, num1):
+        return ('My favourite number is ', num1)
 
 Person_1 = Person('Narek', 'yeranosyan', '31', 'man', False)
 second_person = Person('Shahane', 'Arushanyan', '23', 'woman', False)
-
-print(Person_1.Greeting())
-print(Person_1.Greeting())
-
-
-
-
-
-
+num1 = 5
+print(second_person.Greeting(second_person))
+print(Person_1.Favourite_num(num1))
