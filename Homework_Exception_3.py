2e314aede1178944b782c4a46e341cc0c8664c4e
YeranosyanNarek <narekyeranosyan1111@gmail.com>
2018-10-18 17:09:15 +0400

    Homework_Exception_3

---
+++ Homework_Exception_3.py
@@ -0,0 +1,33 @@
+for i in range(3):
+    try:
+        x = int(input())
+        y = int(input())
+        print(x, '/', y, '=', x / y)
+    except ZeroDivisionError:
+        print('division by zero')
+
+    except ValueError:
+        print("invalid literal for int() with base 10: 'y'")
+    except ValueError:
+        print("invalid literal for int() with base 10: 'x'")
+
+
+
+
+
+
+
+
+
+#ZeroDivisionError: division by zero
+#ValueError: invalid literal for int() with base 10:
+
+
+
+
+
+
+
+
+
+
